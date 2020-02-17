7454e7543a1e028132eaca540820b3a57fee6151
nageshmtech2015 <nageshmtech2015@gmail.com>
2020-02-17 22:05:26 +0530

    Hands on #2:zero values

--- 1-short-declaration-operator/main.go
+++ 1-short-declaration-operator/main.go
@@ -2,11 +2,11 @@ package main
 
 import "fmt"
 
+var x int
+var y string
+var z bool
+
 func main() {
-	x := 42
-	y := "James Bond"
-	z := true
-	fmt.Println(x, y, z)
 	fmt.Println(x)
 	fmt.Println(y)
 	fmt.Println(z)
