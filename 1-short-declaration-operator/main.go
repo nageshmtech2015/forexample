58f99136767bfbc75d3d8fc4928051b20e67208d
nageshmtech2015 <nageshmtech2015@gmail.com>
2020-02-17 22:34:17 +0530

    Hands on #4: creating own type

--- 1-short-declaration-operator/main.go
+++ 1-short-declaration-operator/main.go
@@ -2,11 +2,13 @@ package main
 
 import "fmt"
 
-var x int = 42
-var y string = "James Bond"
-var z bool = true
+type hotdog int
+
+var x hotdog
 
 func main() {
-	s := fmt.Sprintf("%v\t%v\t%v", x, y, z)
-	fmt.Println(s)
+	fmt.Println(x)
+	fmt.Printf("%T\n", x)
+	x = 42
+	fmt.Println(x)
 }
